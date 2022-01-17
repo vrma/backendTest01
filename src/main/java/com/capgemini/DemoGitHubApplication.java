6954221e7f30d2d33182f6015af3e4a568ba56dc
Victor Rafael <vrmachado@gmail.com>
2022-01-17 11:42:22 +0100

    rama master del proyecto

---
+++ src/main/java/com/capgemini/DemoGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitHubApplication.class, args);
+	}
+
+}
